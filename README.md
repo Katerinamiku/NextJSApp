e80aecd56e782376dda69e70725504895cc75c33
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-12-06 17:40:11 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+My first project on nextjs (typescript)
+(CourseHunter project)
---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>My Nextjs App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
