37dc167f12a321c8046c64c3955922d64c17bcc1
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-12-29 13:10:10 +0100

    p&tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { Htag } from "../components/Htag/Htag";
 import { Button } from "../components/Button/Button";
+import { Paragraph } from "../components/Paragrph/Paragraph";
+import { Tag } from "../components/Tag/Tag";
 
 export default function Home(): JSX.Element {
   return (
@@ -11,6 +13,25 @@ export default function Home(): JSX.Element {
       <Button appearance="ghost" arrow="right">
         Button
       </Button>
+      <Paragraph fontSize="small">
+        Some text to demostrate that paragraph is shown.
+      </Paragraph>
+      <Paragraph>Some text to demostrate that paragraph is shown.</Paragraph>
+      <Paragraph fontSize="large">
+        Some text to demostrate that paragraph is shown.
+      </Paragraph>
+      <Tag fontSize="small" color="ghost">
+        small ghost
+      </Tag>
+      <Tag fontSize="large" color="red">
+        large red
+      </Tag>
+      <Tag fontSize="small" color="green" href="#">
+        small green
+      </Tag>
+      <Tag fontSize="small" color="primary" href="#">
+        small primary
+      </Tag>
     </div>
   );
 }
