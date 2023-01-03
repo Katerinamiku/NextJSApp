ac41a1aa2c3d0019e91ffffa8b9b6969f32a3f21
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2023-01-03 17:54:23 +0100

    menu&context

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,6 +1,11 @@
 import { SidebarProps } from "./Sidebar.props";
 import s from "./Sidebar.module.css";
+import { Menu } from "../Menu/Menu";
 
 export const Sidebar = ({ ...rest }: SidebarProps): JSX.Element => {
-  return <div {...rest}>Sidebar</div>;
+  return (
+    <div {...rest}>
+      <Menu />
+    </div>
+  );
 };
