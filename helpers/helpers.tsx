4f10563aab3cc7c39bf7a2edd1db530f6bde80ca
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2023-01-04 17:56:20 +0100

    MenuRoutes&styles

---
+++ helpers/helpers.tsx
@@ -0,0 +1,33 @@
+import { FirstLevelMenuItem } from "../interfaces/menu.interface";
+import { TopLevelCategory } from "../interfaces/topPage.interface";
+import BookIconComponent from "../layout/Menu/icons/BookIconComp";
+import CloudIconComponent from "../layout/Menu/icons/CloudIconComp";
+import CoursesIconComponent from "../layout/Menu/icons/CoursesIconComp";
+import ItemsIconComponent from "../layout/Menu/icons/ItemsIconComp copy";
+
+export const firstLevelMenu: FirstLevelMenuItem[] = [
+  {
+    route: "courses",
+    name: "Courses",
+    icon: <CoursesIconComponent />,
+    id: TopLevelCategory.Courses,
+  },
+  {
+    route: "services",
+    name: "Services",
+    icon: <CloudIconComponent />,
+    id: TopLevelCategory.Services,
+  },
+  {
+    route: "books",
+    name: "Books",
+    icon: <BookIconComponent />,
+    id: TopLevelCategory.Books,
+  },
+  {
+    route: "products",
+    name: "Products",
+    icon: <ItemsIconComponent />,
+    id: TopLevelCategory.Products,
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   };
 };
 
-interface HomeProps extends Record<string, unknown> {
+export interface HomeProps extends Record<string, unknown> {
   menu: MenuItem[];
   firstCategory: number;
 }
