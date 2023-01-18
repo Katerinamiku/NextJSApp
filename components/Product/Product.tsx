f49f1d6315d514464d898f10789e737d9149a65a
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2023-01-18 14:53:58 +0100

    frameMotion animation

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -8,120 +8,127 @@ import { Button } from "../Button/Button";
 import { devlOfNum, priceEur } from "../../helpers/helpers";
 import { Divider } from "./../Divider/Divider";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { ForwardedRef, forwardRef, useRef, useState } from "react";
 import { Review } from "../Review/Review";
 import { ReviewForm } from "../ReviewForm/ReviewForm";
-export const Product = ({
-  product,
-  className,
-  ...rest
-}: ProductProps): JSX.Element => {
-  const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
-  const reviewRef = useRef<HTMLDivElement>(null);
-  const scrollToReview = () => {
-    setIsReviewOpened(true);
-    reviewRef.current?.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  };
-  return (
-    <div className={className} {...rest}>
-      <Card className={s.product}>
-        <div className={s.logo}>
-          <Image
-            src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
-            alt={product.title}
-            width={70}
-            height={70}
-          />
-        </div>
-        <div className={s.title}>{product.title}</div>
-        <div className={s.price}>
-          {priceEur(product.price)}
-          {product.oldPrice && (
-            <Tag color="green" className={s.oldPrice}>
-              {priceEur(product.price - product.oldPrice)}
-            </Tag>
-          )}
-        </div>
+import { motion } from "framer-motion";
 
-        <div className={s.credit}>
-          {priceEur(product.credit)} / <span className={s.months}>months</span>
-        </div>
-        <div className={s.rating}>
-          <Rating rating={product.reviewAvg ?? product.initialRating} />
-        </div>
-        <div className={s.tags}>
-          {product.categories.map((c) => (
-            <Tag color="ghost" key={c} className={s.category}>
-              {c}
-            </Tag>
-          ))}
-        </div>
-        <div className={s.priceTitle}>Price</div>
-        <div className={s.creditTitle}>Credit</div>
-        <div className={s.ratingTitle}>
-          <a href="#ref" onClick={scrollToReview}>
-            {product.reviewCount} {devlOfNum(product.reviewCount)}
-          </a>
-        </div>
-
-        <Divider className={s.hr} />
-
-        <div className={s.description}>{product.description}</div>
-        <div className={s.features}>
-          {product.characteristics.map((c) => (
-            <div className={s.characteristics} key={c.name}>
-              <span className={s.characteristicsName}>{c.name}</span>
-              <span className={s.characteristicsDots}></span>
-              <span className={s.characteristicsValue}>{c.value}</span>
-            </div>
-          ))}
-        </div>
-        <div className={s.advBlock}>
-          {product.advantages && (
-            <div className={s.advantages}>
-              <div className={s.advTitle}>Advantages</div>
-              <div>{product.advantages}</div>
-            </div>
-          )}
-          {product.disadvantages && (
-            <div className={s.disadvantages}>
-              <div className={s.advTitle}>Disadvantages</div>
-              <div>{product.disadvantages}</div>
-            </div>
-          )}
-        </div>
-        <Divider className={cn(s.hr, s.hr2)} />
-        <div className={s.actions}>
-          <Button appearance="primary">Learn more</Button>
-          <Button
-            appearance="ghost"
-            arrow={isReviewOpened ? "down" : "right"}
-            className={s.reviewButton}
-            onClick={() => setIsReviewOpened(!isReviewOpened)}
-          >
-            Reviews
-          </Button>
-        </div>
-      </Card>
-      <Card
-        ref={reviewRef}
-        color="blue"
-        className={cn(s.reviews, {
-          [s.opened]: isReviewOpened,
-          [s.closed]: !isReviewOpened,
-        })}
-      >
-        {product.reviews.map((r) => (
-          <div key={r._id}>
-            <Review review={r} />
-            <Divider />
+const Product = forwardRef(
+  (
+    { product, className, ...rest }: ProductProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ): JSX.Element => {
+    const [isReviewOpened, setIsReviewOpened] = useState<boolean>(false);
+    const reviewRef = useRef<HTMLDivElement>(null);
+    const scrollToReview = () => {
+      setIsReviewOpened(true);
+      reviewRef.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    };
+    return (
+      <div className={className} {...rest} ref={ref}>
+        <Card className={s.product}>
+          <div className={s.logo}>
+            <Image
+              src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
+              alt={product.title}
+              width={70}
+              height={70}
+            />
           </div>
-        ))}
-        <ReviewForm productId={product._id} />
-      </Card>
-    </div>
-  );
-};
+          <div className={s.title}>{product.title}</div>
+          <div className={s.price}>
+            {priceEur(product.price)}
+            {product.oldPrice && (
+              <Tag color="green" className={s.oldPrice}>
+                {priceEur(product.price - product.oldPrice)}
+              </Tag>
+            )}
+          </div>
+
+          <div className={s.credit}>
+            {priceEur(product.credit)} /{" "}
+            <span className={s.months}>months</span>
+          </div>
+          <div className={s.rating}>
+            <Rating rating={product.reviewAvg ?? product.initialRating} />
+          </div>
+          <div className={s.tags}>
+            {product.categories.map((c) => (
+              <Tag color="ghost" key={c} className={s.category}>
+                {c}
+              </Tag>
+            ))}
+          </div>
+          <div className={s.priceTitle}>Price</div>
+          <div className={s.creditTitle}>Credit</div>
+          <div className={s.ratingTitle}>
+            <a href="#ref" onClick={scrollToReview}>
+              {product.reviewCount} {devlOfNum(product.reviewCount)}
+            </a>
+          </div>
+
+          <Divider className={s.hr} />
+
+          <div className={s.description}>{product.description}</div>
+          <div className={s.features}>
+            {product.characteristics.map((c) => (
+              <div className={s.characteristics} key={c.name}>
+                <span className={s.characteristicsName}>{c.name}</span>
+                <span className={s.characteristicsDots}></span>
+                <span className={s.characteristicsValue}>{c.value}</span>
+              </div>
+            ))}
+          </div>
+          <div className={s.advBlock}>
+            {product.advantages && (
+              <div className={s.advantages}>
+                <div className={s.advTitle}>Advantages</div>
+                <div>{product.advantages}</div>
+              </div>
+            )}
+            {product.disadvantages && (
+              <div className={s.disadvantages}>
+                <div className={s.advTitle}>Disadvantages</div>
+                <div>{product.disadvantages}</div>
+              </div>
+            )}
+          </div>
+          <Divider className={cn(s.hr, s.hr2)} />
+          <div className={s.actions}>
+            <Button appearance="primary">Learn more</Button>
+            <Button
+              appearance="ghost"
+              arrow={isReviewOpened ? "down" : "right"}
+              className={s.reviewButton}
+              onClick={() => setIsReviewOpened(!isReviewOpened)}
+            >
+              Reviews
+            </Button>
+          </div>
+        </Card>
+        <Card
+          ref={reviewRef}
+          color="blue"
+          className={cn(s.reviews, {
+            [s.opened]: isReviewOpened,
+            [s.closed]: !isReviewOpened,
+          })}
+        >
+          {product.reviews.map((r) => (
+            <div key={r._id}>
+              <Review review={r} />
+              <Divider />
+            </div>
+          ))}
+          <ReviewForm productId={product._id} />
+        </Card>
+      </div>
+    );
+  }
+);
+Product.displayName = "Product";
+
+export default motion(Product);
--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -9,6 +9,7 @@ import {
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { firstLevelMenu } from "../../helpers/helpers";
+import { motion } from "framer-motion";
 
 export const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(MyAppContext);
@@ -66,13 +67,17 @@ export const Menu = (): JSX.Element => {
               >
                 {m._id.secondCategory}
               </div>
-              <div
+              <motion.div
+                layout
+                variants={variants}
+                initial={m.isOpened ? "visible" : "hidden"}
+                animate={m.isOpened ? "visible" : "hidden"}
                 className={cn(s.secondLevelBlock, {
                   [s.secondLevelBlockOpened]: m.isOpened,
                 })}
               >
                 {buildThirdevel(m.pages, menuItem.route)}
-              </div>
+              </motion.div>
             </div>
           );
         })}
@@ -81,17 +86,40 @@ export const Menu = (): JSX.Element => {
   };
   const buildThirdevel = (pages: PageItem[], route: string) => {
     return pages.map((p) => (
-      <Link href={`/${route}/${p.alias}`} key={p._id}>
-        <a
-          className={cn(s.thirdLevel, {
-            [s.thirdLevelActive]: `${route}/${p.alias}` === router.asPath,
-          })}
-        >
-          {p.category}
-        </a>
-      </Link>
+      <motion.div key={p._id} variants={variantsChildren}>
+        <Link href={`/${route}/${p.alias}`}>
+          <a
+            className={cn(s.thirdLevel, {
+              [s.thirdLevelActive]: `${route}/${p.alias}` === router.asPath,
+            })}
+          >
+            {p.category}
+          </a>
+        </Link>
+      </motion.div>
     ));
   };
 
   return <div className={s.menu}>{buildFirstLevel()}</div>;
+};
+//-------animation-------
+const variants = {
+  visible: {
+    marginBottom: 20,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.08,
+    },
+  },
+  hidden: { marginBottom: 0 },
+};
+const variantsChildren = {
+  visible: {
+    opacity: 1,
+    height: 29,
+  },
+  hidden: {
+    opacity: 0,
+    height: 0,
+  },
 };
