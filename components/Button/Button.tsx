732385ed9f4800992dda9a98569dd0891fd5dff3
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2023-01-19 17:12:24 +0100

    colorAccesability

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,10 @@
 import { ButtonProps } from "./Button.props";
 import s from "./Button.module.css";
 import cn from "classnames";
-import ArrowIcon from "./right-arrow.svg";
+
 import ArrowIconComponent from "./ArrowIconComponent";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
 export const Button = ({
   appearance,
@@ -11,12 +13,18 @@ export const Button = ({
   arrow = "none",
   ...rest
 }: ButtonProps): JSX.Element => {
+  const scale = useMotionValue(1);
+  useEffect(() => {
+    scale.onChange((s) => console.log(s));
+  }, []);
   return (
-    <button
+    <motion.button
+      whileHover={{ scale: 1.05 }}
       className={cn(s.button, className, {
         [s.primary]: appearance === "primary",
         [s.ghost]: appearance === "ghost",
       })}
+      style={{ scale }}
       {...rest}
     >
       {children}
@@ -31,6 +39,6 @@ export const Button = ({
           {/* <ArrowIcon /> */}
         </span>
       )}
-    </button>
+    </motion.button>
   );
 };
